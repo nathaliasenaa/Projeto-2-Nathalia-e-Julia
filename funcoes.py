727212f48c3b4024ae40c42713e3b128055907d9
Julia Zanin <juliapz1@al.insper.edu.br>
2026-04-24 09:41:31 -0300

    exercicio4

--- funcoes.py
+++ funcoes.py
@@ -19,6 +19,19 @@ def remover_dado (dados_rolados, dados_estoque, indice):
     dados_estoque.pop(indice)
     return [dados_rolados, dados_estoque]
 
+def calcula_pontos_regra_simples (faces_dados_rolados):
+    calcula_pontos= {
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0
+    }
+    for i in faces_dados_rolados:
+        calcula_pontos[i]+=i
+    return calcula_pontos
+
 
 
 
